46e216c995aeec90c11b2e2eb9fb1c8252ab719a
leonsokolowski <leonsokolowski2104@gmail.com>
2023-10-28 17:26:36 -0300

    parcial

--- Parcial/clase_equipo.py
+++ Parcial/clase_equipo.py
@@ -4,12 +4,16 @@ class Equipo:
         self.archivo = archivo
         self.lista_de_jugadores = lista_de_jugadores
     
-    def importar_json (self):
+    def importar_json (self, clave):
         with open(self.archivo, "r", encoding= "utf-8") as jugadores_dream_team:
             objeto = json.load(jugadores_dream_team)
-            return objeto
-    
-    
+            jugadores = objeto.get(clave)
+            for jugador in jugadores:
+                self.lista_de_jugadores.append(jugador)
+            return self.lista_de_jugadores
+        
              
 dream_team = Equipo("Parcial/dream_team.json", [])
-print (dream_team.importar_json())
+print (dream_team.importar_json("jugadores"))
+
+
--- Parcial/clase_jugador.py
+++ Parcial/clase_jugador.py
@@ -1,5 +1,9 @@
 from clase_equipo import Equipo
 
 class Jugador(Equipo):
-    def __init__(self, archivo, nombre, posicion, lista_de_logros):
-        super().__init__(archivo)
+    def __init__(self, archivo, lista_de_jugadores, nombre, posicion, lista_de_logros):
+        super().__init__(archivo, lista_de_jugadores)
+        self.nombre = nombre
+        self.posicion = posicion
+        self.lista_de_logros = lista_de_logros
+    
