a7919ab34f74d1a89a78c98e4adfe8d8d6dc40dc
leonsokolowski <leonsokolowski2104@gmail.com>
2023-10-31 14:29:47 -0300

    parcial

--- Parcial/temporal.py
+++ Parcial/temporal.py
@@ -16,3 +16,26 @@
 #     8. Salir.
 #     """
 #     print(menu)
+
+lista_flotantes = [-6.0, -3.0, 1.0, 4.0, 6.0, 7.0, 8.0, 9.0]
+
+def quick_sort (lista : list[float]):
+    
+    if len(lista) < 2:
+        return lista
+    else:
+        lista_a_ordenar = lista.copy()
+        pivot = lista_a_ordenar.pop(0)
+        numeros_mas_grandes = []
+        numeros_mas_chicos = []
+        
+        for numero in lista_a_ordenar:
+            if numero > pivot:
+                numeros_mas_grandes.append(numero)
+            elif numero <= pivot:
+                numeros_mas_chicos.append(numero)
+        
+    return quick_sort(numeros_mas_grandes) + [pivot] + quick_sort(numeros_mas_chicos)
+
+print(quick_sort(lista_flotantes))
+
